c105b433bf0b52d061417bb5b946013e02d7f8b0
Danilo da Silva Ataide <62820994+danilosataide@users.noreply.github.com>
2020-09-16 13:17:58 -0300

    entidades da aplicação e migrations do BD

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -6,6 +6,12 @@ const connection = knex({
     connection: {
         filename: path.resolve(__dirname, 'database.sqlite'),   
     },
+    useNullAsDefault: true,
 });
 
 export default connection;
+
+// Migrations = Histórico do banco de dados
+
+// create table points  
+// create table users 
