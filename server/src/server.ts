bffc2f2546f838cda4fd53637594a8a5464d652a
Danilo da Silva Ataide <62820994+danilosataide@users.noreply.github.com>
2020-09-15 22:24:39 -0300

    explorando rotas e recursos com Insomnia

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,20 +2,64 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+// Rota: Endereço completo da requisição
+// Recurso: Qual entidade estamos acessando do sistema
+
+// GET: Buscar uma ou mais informações do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Atualizar uma informação existente no back-end
+// DELETE: Remover uma informação do back-end
+
+// POST http://localhost:3333/users = Criar um usuário
+// GET http://localhost:3333/users = Listar usuários
+// GET http://localhost:3333/users/5 = Buscar dados do usuário com ID 5
+
+// Request Param: Parâmetros que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação
+// Request Body: Parâmetros para criação/atualização de informações 
+
+// SELECT * FROM users WHERE name = 'Danilo'
+// knex('users').where('name', 'Danilo').select('*')
+
+const users = [
+    'Danilo',
+    'Maria',
+    'Robson',
+    'José',
+    'Daniel',
+    'Marta',
+    'Roberto'
+];
+
 app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+    const search = String(request.query.search);
+    
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    
+    return response.json(filteredUsers);
+});
 
-    // JSON
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
 
-    response.json([
-        'Danilo',
-        'Maria',
-        'Robson',
-        'José',
-        'Daniel',
-        'Marta',
-        'Roberto'
-    ]);
+    const user = users[id];
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    console.log(data);
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return response.json(user);
+
 });
 
 app.listen(3333);
