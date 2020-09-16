1e649c55c7f0c5b6943e16bcf2c84e414cf27742
Danilo da Silva Ataide <62820994+danilosataide@users.noreply.github.com>
2020-09-16 16:21:00 -0300

    rotas/controllers das funcionalidades da aplicação

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+// import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/itemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello Word'});
-});
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
+
+// Service Pattern
+// Repository Pattern
