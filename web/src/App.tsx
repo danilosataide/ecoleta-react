7136cafa7fdb66cbb7db75fc9b6dde5108e4e544
Danilo da Silva Ataide <62820994+danilosataide@users.noreply.github.com>
2020-09-17 12:49:04 -0300

    estado e imutabilidade

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { count } from 'console';
+import React, { useState } from 'react';
 import './App.css';
 
 // JSX: Sintaxe de XML dentro do JavaScript
@@ -6,11 +7,19 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0); // ['valor do estado', função para atualizar o valor do estado ]
+
+  function handleButtonClick(){
+    setCounter(counter + 1);
+  }
+
   return (
     <div>
       <Header title="Ecoldeta" />
 
-      <h1>conteúdo da aplicação</h1> 
+      <h1>conteúdo da aplicação {`Contador: ${counter}`}</h1> 
+      <h1>{counter}</h1> 
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
 }
