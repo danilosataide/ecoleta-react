5c63ee6aae61d4361ec6755c37c6c8f159545dcc
Danilo da Silva Ataide <62820994+danilosataide@users.noreply.github.com>
2020-09-16 23:49:55 -0300

    Limpeza da aplicação web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>TITLE</h1>
-      </header>
-    </div>
+    <h1>Hello World</h1> 
   );
 }
 
