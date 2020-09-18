9821312b01a7c98582ba7f0d0becd88a248b6e16
Danilo da Silva Ataide <62820994+danilosataide@users.noreply.github.com>
2020-09-17 22:12:25 -0300

    opção para selecionar um ponto no mapa

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import api from '../../services/api';
+import { LeafletMouseEvent } from 'leaflet';
 import axios from 'axios';
 
 import './styles.css';
@@ -30,6 +31,7 @@ const CreatePoint = () => {
 
     const [selectedUf, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
+    const [selectedPosition, setSelectedPosition] = useState<[ number, number]>([0, 0]);
 
     useEffect(() => {
         api.get('items').then(response => {
@@ -66,6 +68,13 @@ const CreatePoint = () => {
     function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
         const city = event.target.value;
         setSelectedCity(city);
+    }
+
+    function handleMapClick(event: LeafletMouseEvent) {
+        setSelectedPosition([
+            event.latlng.lat,
+            event.latlng.lng
+        ]);
     }
 
     return (
@@ -121,16 +130,13 @@ const CreatePoint = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
-                    <Map center={ [-23.4721805, -47.4666742] } zoom={15} >
+                    <Map center={ [-23.4721805, -47.4666742] } zoom={15} onClick={handleMapClick}>
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
 
-                        <Marker position={ [-23.4721805, -47.4666742] }>
-                        
-                        </Marker>
-
+                        <Marker position={ selectedPosition }/>
                     </Map>
 
                     <div className="field-group">
